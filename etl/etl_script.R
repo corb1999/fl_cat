6b4261190c19463fe0e11afa6fed83a263e1fa5c
corb1999 <le.corbusier.29845@gmail.com>
2022-06-18 20:34:20 -0400

    downloading excel files of past cat rates. writing the etl to consume the rate files and clean into an analyzable state.

--- etl/etl_script.R
+++ etl/etl_script.R
@@ -21,6 +21,8 @@ library(lubridate)
 library(scales)
 # library(gt)
 library(janitor)
+library(readxl)
+library(tidycensus)
 set.seed(metadatar$seed_set[1])
 options(digits = 4, max.print = 99, warnPartialMatchDollar = TRUE, 
         tibble.print_max = 30, scipen = 999, nwarnings = 5, 
@@ -96,30 +98,119 @@ cash_money <- function(x) {
 # ^ ====================================
 # :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
-# read raw dataset -----------------------------------------------
+# unionizer script --------------------------------------------
+# a script to load and UNION ALL a number of datasets together
+# require(purrr)
 
-# load a csv file
-loader_path1 <- paste0(getwd(), "/etl/ore/raw_data.csv")
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# identify dropzone where files are stored and vector the filenames
+filepath_prefix_payload <- paste0(getwd(), "/etl/ore/cat_fund_rates")
+
+# put the files list into a dataframe
+payload <- data.frame(file_nm = list.files(path = filepath_prefix_payload)) %>% 
+  mutate(file_nm_full = paste0(filepath_prefix_payload, "/", 
+                               file_nm)) %>% 
+  mutate(rate_yr = str_sub(file_nm, start = 1L, end = 2))
+
+# test +++++++++++++++++++++++++++
+dim(payload)
+# payload
+# payload[1, 1]
+# payload[1, 2]
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# write a function to read each file the same way into r
+fun_readfiles <- function(filepaths, arg_rate_yr) {
+  sub_arg_sheets <- c('Com 90', 'Com 75', 'Com 45')
+  sub_arg_range <- rep('A9:H34', length(sub_arg_sheets))
+  # set up a fun to read each sheet
+  fun_sub_read <- function(sub_arg_filepaths, sub_arg_rate_yr, 
+                           arg_sheet, arg_range) {
+    xx <- read_excel(path = sub_arg_filepaths, 
+                     sheet = arg_sheet, 
+                     range = arg_range)
+    xx <- xx %>% 
+      mutate(rate_yr = as.integer(sub_arg_rate_yr) + 2000, 
+             lob_cov_lvl = arg_sheet, 
+             ded_lvl = 'DED 0000-2500') 
+    names(xx)[1] <- 'zip_code_group'
+    names(xx)[8] <- 'UNK Construction'
+    return(xx)
+  }
+  # now read each sheet in each year's workbook
+  xx <- pmap_dfr(list(filepaths, arg_rate_yr, 
+                      sub_arg_sheets, sub_arg_range), 
+                 fun_sub_read)
+  return(xx)}
+
+# test +++++++++++++++++++++++++++
+# fun_readfiles(payload[2, 2], payload[2, 3])
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# execute the file reading purrr, and time the execution
 clockin()
-raw_df <- read.csv(loader_path1, stringsAsFactors = FALSE)
+payload_list <- pmap(list(payload[, 2], payload[, 3]), 
+                     .f = fun_readfiles)
 clockout()
-dim(raw_df)
 
-# ^ -----
+# test +++++++++++++++++++++++++++
+# payload_list[[1]]
+# dim(payload_list[[1]])
 
-# clean the dataset -------------------------------------------
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# run checks and validation on the loaded files
+payload_stats <- data.frame(col_num_chk = map_dbl(payload_list, 
+                                                  ncol), 
+                            row_num_chk = map_dbl(payload_list, 
+                                                  nrow))
 
-# cleaning
+fun_colnames_chk <- function(x) {
+  aa <- colnames(x)
+  bb <- reduce(aa, paste0)
+  return(bb)}
+
+# perform checks !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+payload_stats
+length(unique(payload_stats[, 1])) == 1
+length(unique(map_chr(payload_list, fun_colnames_chk))) == 1
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# combine into a single dataframe, as long as checks pass
 clockin()
-dfa <- raw_df %>% clean_names() %>% as_tibble()
+df <- map_dfr(payload_list, rbind)
 clockout()
 
-# cleanup !!!!!!!!!!!!!!!!!!!
-rm(raw_df)
-ls()
-trash()
+# test +++++++++++++++++++++++++++
+dim(df)
 
-# ^ ----- 
+# perform checks !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+sum(payload_stats$row_num_chk) == nrow(df)
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# execute any additional filter and manipulation before writing
+
+df <- df %>% 
+  pivot_longer(cols = c(2:8), 
+               names_to = 'construct_type', 
+               values_to = 'rate_per_1k') %>% 
+  mutate(lob = str_sub(lob_cov_lvl, start = 1L, end = 3), 
+         cov_lvl = str_sub(lob_cov_lvl, start = -2, end = -1), 
+         cov_lvl = as.double(cov_lvl) / 100) %>% 
+  select(-lob_cov_lvl)
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+# create and append a metadata tag
+metadata_tag <- paste0("unionizer script metadata; ", 
+                       "files consumed = ", 
+                       nrow(payload_stats), 
+                       "; runtime = ", Sys.time(), 
+                       "; nrow = ", nrow(df), 
+                       "; ncol = ", ncol(df))
+metadata_tag
+df <- df %>% mutate(metadata_tag = "NA")
+df[1, "metadata_tag"] <- metadata_tag
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 # write the cleaned dataset ---------------------------------------
 
